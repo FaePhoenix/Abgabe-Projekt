c493798649ac705d4b3257f8711e34249694de44
fae <fae.koerper@hamburg.de>
2025-08-27 15:06:52 +0200

    move testing into funcs

--- src/testing.py
+++ src/testing.py
@@ -4,9 +4,29 @@ from cycles.tarjan_calculator import TarjanCalculator
 
 def main() -> int:
     #Just used for testing out new components and how they work together with old components
-    helper = FileHelper()
-    graph = helper.read_graph_from_file(r"C:\Users\fae\Documents\Uni\Semester6\Skriptsprachen\Abgabe-Projekt\txtfiles\graph500.txt")
+    py_testing()
+    return 0
 
+
+def py_testing() -> None:
+    links = ["a", "b", "c", "d"]
+    entries = ["a", "e", "d"]
+
+    if (name := entries[0]) in links:
+        print(f"Found {name}")
+
+    return None
+
+
+
+def cycle_testing() -> None:
+    helper = FileHelper("")
+    graphfile = r"C:\Users\fae\Documents\Uni\Semester6\Skriptsprachen\Abgabe-Projekt\txtfiles\graph500.txt"
+    graph = helper.read_graph_from_file(graphfile, False)
+
+    if not graph:
+        print("Failed to read graph")
+        return None
 
     calc = TarjanCalculator()
     cycles = calc.find_strongly_connected_components(graph)
@@ -14,8 +34,7 @@ def main() -> int:
     for cycle in cycles:
         print(cycle)
 
-    return 0
-
+    return None
 
 if __name__ == "__main__":
     main()
